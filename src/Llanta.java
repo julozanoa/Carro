e036fe7bce45fe161cb7eeffc4cbc51b95174cc7
Estudiante <Estudiante@BE401S103C02ING.unal.loc>
2019-04-24 08:52:07 -0500

    first thirth

---
+++ src/Llanta.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Llanta {
+    private String marca;
+    private double diametro;
+
+    public Llanta(String marca, double diametro) {
+        this.marca = marca;
+        this.diametro = diametro;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getDiametro() {
+        return diametro;
+    }
+
+    public void setDiametro(double diametro) {
+        this.diametro = diametro;
+    }
+
+    
+    
+    
+}
